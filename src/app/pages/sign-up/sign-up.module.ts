217e313cd67ff43a167ddb1e6a85cf82b3002705
Amit Tiwari <amitakash18@gmail.com>
2020-10-16 21:30:40 +0530

    add form modules and create signup form and  done validation

--- src/app/pages/sign-up/sign-up.module.ts
+++ src/app/pages/sign-up/sign-up.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { SignUpPageRoutingModule } from './sign-up-routing.module';
 
 import { SignUpPage } from './sign-up.page';
@@ -11,10 +10,11 @@ import { SignUpPage } from './sign-up.page';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
     SignUpPageRoutingModule
   ],
   declarations: [SignUpPage]
 })
-export class SignUpPageModule {}
+export class SignUpPageModule { }
--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  public signUpForm: FormGroup;
+  public isSubmitted: boolean = false;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+      privacy: [false, Validators.pattern('true')]
+    })
+  }
+
+  private checkPwd(formData) {
+    const pwd = formData['controls']['password'].value;
+    const cPwd = formData['controls']['confirmPassword'].value;
+
+    if (pwd !== cPwd) {
+      formData['controls']['confirmPassword'].setErrors({ 'different': true });
+    }
+  }
+
+  get errorControl() {
+    return this.signUpForm.controls;
+  }
+  /**
+   * Method to use to create account for a user
+   * @param form : signUpForm controls details
+   */
+  public signUp(formData) {
+    this.checkPwd(formData);
+    if (this.signUpForm.invalid) {
+      this.isSubmitted = true;
+      return;
+    }
   }
 
 }
